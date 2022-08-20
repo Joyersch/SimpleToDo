c6f32887c8fbe78b1653e9ed45dd3b63549877fa
Erik Schnittker <[omitted]>
2022-08-21 01:33:24 +0200

    Fixed checkboxes being broken do to missing quotation maks

--- classes/view/Checkbox.php
+++ classes/view/Checkbox.php
@@ -12,7 +12,7 @@ class Checkbox
         !isset($this -> checked))
             return "";
 
-        $checkbox = @"<input type='checkbox' id='entry_{$this -> id}' name='{$this -> id}' onclick=' return redirectToChecked({$this -> id},document.getElementById(\"entry_{$this -> id}\").checked,$authentication)'";
+        $checkbox = @"<input type='checkbox' id='entry_{$this -> id}' name='{$this -> id}' onclick=' return redirectToChecked({$this -> id},document.getElementById(\"entry_{$this -> id}\").checked,\"{$authentication}\")'";
         if ($this -> checked)
             $checkbox .= "checked";
         $checkbox .= ">";
